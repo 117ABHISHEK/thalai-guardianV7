d7d5c72766a5acc3e1fa28403ec6ff6ba7b2b960
Abhishek Chaugule <147874160+117ABHISHEK@users.noreply.github.com>
2025-11-19 10:38:17 +0530

    css work

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -4,7 +4,7 @@ const User = require("../models/User")
 const authMiddleware = async (req, res, next) => {
   try {
     const token = req.cookies.token
-
+    
     if (!token) {
       return res.status(401).json({ message: "No token, authorization denied" })
     }
